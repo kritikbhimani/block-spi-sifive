8a356a8ec2fbafa6d22154f880ff58ae17f026ca
kritik bhimani <kritik.bhimani@sifive.com>
2019-10-30 15:13:46 +0530

    add uart infra

---
+++ craft/pio/src/uart.scala
@@ -0,0 +1,49 @@
+package sifive.blocks.pio
+
+import chisel3._
+// import chisel3.{withClockAndReset, _}
+import chisel3.util._
+import chisel3.experimental._
+
+import freechips.rocketchip.config._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.amba.axi4._
+import freechips.rocketchip.amba.apb._
+import freechips.rocketchip.amba.ahb._
+import freechips.rocketchip.interrupts._
+import freechips.rocketchip.util.{ElaborationArtefacts}
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.regmapper._
+
+import sifive.skeleton._
+
+
+
+
+class SkeletonUartDUT(harness: LazyScope)(implicit p: Parameters) extends SkeletonDUT(harness)
+{
+	val uartAttachParams = UARTAttachParams(
+	    UARTParams(address = 0x10013000,
+	      dataBits = 9,
+	      includeFourWire = true,
+	      includeParity = true,
+	      includeIndependentParity = false),
+	    19,
+	    cbus,
+	    ibus.fromSync)
+	val uart = UART.attach(uartAttachParams)
+	val uartNode = BundleBridgeSink[UARTPortIO]()
+	uartNode := uart.ioNode
+
+	InModuleBody {	UART.loopback(uartNode.bundle)	}
+}
+
+class SkeletonUartTestHarness()(implicit p: Parameters) extends LazyModule with LazyScope
+{
+  val dut = LazyModule(new SkeletonUartDUT(this))
+  lazy val module = new LazyModuleImp(this) {
+    ConstructOM.constructOM()
+    Debug.tieoffDebug(dut.module.debug)
+  }
+}
